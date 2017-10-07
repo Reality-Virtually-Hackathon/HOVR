4d1e42187970d57c24243f025cbed7e49e13670d
Peter Iordanov <iordano2@illinois.edu>
2017-10-07 18:01:16 -0400

    found controllers

--- Assets/Script/GameObjectNameConstants.cs
+++ Assets/Script/GameObjectNameConstants.cs
@@ -3,5 +3,7 @@
 	public static string PlayerBaseName = "[CameraRig]";
 	public static string PedalTrackerName = "Pedal Tracker (right)";
 	public static string ScriptsObject= "ScriptsManager";
+    public static string LeftController = "[CameraRig]/Controller (left)";
+    public static string RightController = "[CameraRig]/Controller (right)";
 
 }
