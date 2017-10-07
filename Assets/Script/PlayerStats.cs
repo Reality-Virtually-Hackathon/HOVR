3db2fe7bf3cd12e6bd93f8e5f99d8e2e4261918b
David Gu <david.gu@hotmail.com>
2017-10-07 09:54:49 -0400

    Added bits to playerstats

--- Assets/Script/PlayerStats.cs
+++ Assets/Script/PlayerStats.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerStats {
 
 	public float points;
+	public float distanceTraveled;
 	List<float> sessionVelocities;
 	GameObject scripts;
 	GameObject playerBase;
@@ -18,20 +19,27 @@ public class PlayerStats {
 	
 	// Update is called once per frame
 	void Update () {
-
 		//Get current velocity and save it to the list
 		float currentVelocity = playerBase.GetComponent<Rigidbody>().velocity.x; //TODO: verify what the forward velocity is
 		sessionVelocities.Add(currentVelocity);
+
 	}
 
 	void loadPlayerData (){
 		//Called at the beginning of main menu load. Pulls data from playerprefs into some sort of display?
 		//TODO: Load past player data and stats from playerprefs
 
+
+
 	}
 
 	void saveSessionData (){
 		//Called at the end of a play session. Saves the statistics from the current play session to playerprefs.
+		//Turn the session data into a json string 
+		//Call a playerprefs.setstring
+
+
+
 	}
 
 }
