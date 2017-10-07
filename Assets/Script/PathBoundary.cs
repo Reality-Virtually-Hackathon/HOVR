2129f9d0e3059375c2e4b262efc16fad288b0758
David Gu <david.gu@hotmail.com>
2017-10-07 13:43:16 -0400

    Added PathBoundary

---
+++ Assets/Script/PathBoundary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBoundary : MonoBehaviour {
+
+	GameObject PlayerBase;
+
+
+	void Start(){
+		PlayerBase = GameObject.Find (GameObjectNameConstants.PlayerBaseName);
+	}
+
+	//This script is attached to objects that form the boundaries of the path
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag = "player") {
+			PlayerBase.GetComponent<StayOnPath> ().onPath = false;
+		}
+	}
+
+	void OnCollisionExit(Collision collision){
+		if (collision.gameObject.tag = "player") {
+			PlayerBase.GetComponent<StayOnPath> ().onPath = true;
+		}
+	}
+}
