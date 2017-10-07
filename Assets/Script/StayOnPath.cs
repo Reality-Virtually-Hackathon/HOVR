40257385229a897ffda5c685fd624b827fa451e2
David Gu <david.gu@hotmail.com>
2017-10-07 13:29:25 -0400

    Added StayOnPath

---
+++ Assets/Script/StayOnPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayOnPath : MonoBehaviour {
+
+	GameObject visionFader;
+	float visionBlockerLevel = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CheckPosition ()) {
+			visionBlockerLevel = 0f;	
+		} else {
+			visionBlockerLevel += 0.1f;
+		}
+
+	}
+
+	//Check if the player is currently within the approved area
+	bool CheckPosition(){
+		
+
+	}
+}
