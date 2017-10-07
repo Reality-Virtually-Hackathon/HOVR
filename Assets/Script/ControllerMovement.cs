2fb3ce5ff0b4066ff0412772582e37ab9f1296b1
Peter Iordanov <iordano2@illinois.edu>
2017-10-07 18:33:41 -0400

    fix controller code after merge

--- Assets/Script/ControllerMovement.cs
+++ Assets/Script/ControllerMovement.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ControllerMovement : MonoBehaviour {
 
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
     private void Awake()
     {
-        
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        Debug.Assert(trackedObj != null);
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (Controller.GetHairTriggerDown())
+        {
+            Debug.Log(gameObject.name + " Trigger Press");
+        }
+    }
 }
--- Assets/Script/PedalMovementManager.cs
+++ Assets/Script/PedalMovementManager.cs
@@ -7,17 +7,6 @@ public class PedalMovementManager : MonoBehaviour {
 	public GameObject pedalTrackerLeft, pedalTrackerRight;
 	public GameObject playerBase;
 
-    private SteamVR_TrackedObject lHandController, rHandController;
-
-    private SteamVR_Controller.Device LController
-    {
-        get { return SteamVR_Controller.Input((int)lHandController.index); }
-    }
-    private SteamVR_Controller.Device RController
-    {
-        get { return SteamVR_Controller.Input((int)rHandController.index); }
-    }
-
     float totalDistanceTraveled;
 	Vector3 previousLoc;
 	float pedalDistanceVelocityMultiplier = 1.0f;
@@ -33,18 +22,10 @@ public class PedalMovementManager : MonoBehaviour {
 
     }
 
-    void Awake()
-    {
-        GameObject b = GameObject.Find(GameObjectNameConstants.PlayerBaseName);
-        GameObject lH = b.transform.Find("Controller (left)").gameObject;
-        Debug.Log(lH);
+	
+	// Update is called once per frame
+	void Update () {
 
-        lHandController = GameObject.Find(GameObjectNameConstants.LeftController).GetComponent<SteamVR_TrackedObject>();
-        rHandController = GameObject.Find(GameObjectNameConstants.RightController).GetComponent<SteamVR_TrackedObject>();
-    }
-
-    void CheckSteps()
-    {
         float tickDistanceTraveled = 0.0f;
 
         Vector3 currentLoc = pedalTrackerLeft.transform.localPosition;
@@ -59,26 +40,6 @@ public class PedalMovementManager : MonoBehaviour {
         }
 
         previousLoc = currentLoc;
+
     }
-
-    void CheckTurning()
-    {
-        if (LController.GetHairTriggerDown())
-        {
-            Debug.Log(gameObject.name + " LTrigger Press");
-        }
-        if (RController.GetHairTriggerDown())
-        {
-            Debug.Log(gameObject.name + " RTrigger Press");
-        }
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
-        CheckSteps();
-        CheckTurning();
-
-
-	}
 }
