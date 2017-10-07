6b8463f7209898966b22d2c897aef3642b3521a6
David Gu <david.gu@hotmail.com>
2017-10-07 14:43:30 -0400

    renamed file

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader: MonoBehaviour {
+
+	public void StartSession(){
+		//Float out UI, initialize variables 
+
+
+
+	}
+
+}
