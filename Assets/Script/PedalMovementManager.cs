9e2c6fd622ea9de297f5a90b5a1176918ecf81b0
Peter Iordanov <iordano2@illinois.edu>
2017-10-07 11:47:47 -0400

    initial playercontroller

--- Assets/Script/PedalMovementManager.cs
+++ Assets/Script/PedalMovementManager.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PedalMovementManager : MonoBehaviour {
 
-	public GameObject pedalTracker;
+	public GameObject pedalTrackerLeft, pedalTrackerRight;
 	public GameObject playerBase;
+
+
 	float totalDistanceTraveled;
 	Vector3 previousLoc;
-
 	float pedalDistanceVelocityMultiplier = 1.0f;
 
 	// Use this for initialization
 	void Start () {
-		if (pedalTracker != null) {
-			previousLoc = pedalTracker.transform.localPosition;
+		
+		if (pedalTrackerLeft != null && pedalTrackerRight != null) {
+			previousLoc = pedalTrackerLeft.transform.localPosition;
 		} else {
 			Debug.Log ("PedalMovementManager Initializiation Error: Could not find pedal tracker");
 		}
@@ -24,14 +26,13 @@ public class PedalMovementManager : MonoBehaviour {
 	void Update () {
 		float tickDistanceTraveled = 0.0f;
 
-		Vector3 currentLoc = pedalTracker.transform.localPosition;
+		Vector3 currentLoc = pedalTrackerLeft.transform.localPosition;
 		tickDistanceTraveled = Vector3.Distance (previousLoc, currentLoc);
 		totalDistanceTraveled += tickDistanceTraveled;
 
-		//TODO: Apply force to player and increase velocity
-		Vector3 playerForward = playerBase.transform.forward; //This may not work, may need Camera.forward
+		Vector3 playerForward = playerBase.transform.forward;
 
-		GameObject.Find (GameObjectNameConstants.PlayerBaseName).GetComponent<PlayerMovement> ().AddVelocity (playerForward * pedalDistanceVelocityMultiplier);
+		playerBase.GetComponent<PlayerMovement> ().AddVelocity (playerForward * pedalDistanceVelocityMultiplier);
 
 	}
 }
