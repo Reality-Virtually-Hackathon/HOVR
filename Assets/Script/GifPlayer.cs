afa890e6ee1194bbf831efede2c7205e285d82fb
David Gu <david.gu@hotmail.com>
2017-10-08 17:44:33 -0400

    removed canvas

--- Assets/Script/GifPlayer.cs
+++ Assets/Script/GifPlayer.cs
@@ -1,56 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class GifPlayer : MonoBehaviour {
+//public class GifPlayer : MonoBehaviour {
 
-    public Sprite[] gif;
-	public Image displayImage;
-	public bool cycling = false;
+//    public Sprite[] gif;
+//	public Image displayImage;
+//	public bool cycling = false;
 
 
-	// Use this for initialization
-	void Start () {
-        gif = new Sprite[138];
-		for (int i = 0; i < 137; i++) {
-			if (i < 10) {
-				gif [i] = Resources.Load ("Gif/instructions_0000" + i) as Sprite;
-			} else if (i < 100) {
-				gif [i] = Resources.Load ("Gif/instructions_000" + i) as Sprite;
-			} else {
-				gif [i] = Resources.Load ("Gif/instructions_00" + i) as Sprite;
-			}
+//	// Use this for initialization
+//	void Start () {
+//        gif = new Sprite[138];
+//		for (int i = 0; i < 137; i++) {
+//			if (i < 10) {
+//				gif [i] = Resources.Load ("Gif/instructions_0000" + i) as Sprite;
+//			} else if (i < 100) {
+//				gif [i] = Resources.Load ("Gif/instructions_000" + i) as Sprite;
+//			} else {
+//				gif [i] = Resources.Load ("Gif/instructions_00" + i) as Sprite;
+//			}
 			
-		}
+//		}
 
 		
-	}
+//	}
 
-    IEnumerator playGif()
-    {
-        for(int i = 0; i<=137; i++)
-        {
-	        displayImage.GetComponent<Image>().sprite = gif[i];
-            yield return new WaitForSeconds(0.04f);
-        }
+//    IEnumerator playGif()
+//    {
+//        for(int i = 0; i<=137; i++)
+//        {
+//	        displayImage.GetComponent<Image>().sprite = gif[i];
+//            yield return new WaitForSeconds(0.04f);
+//        }
 
-    }
+//    }
 
-    private void OnEnable()
-    {
-		playGif ();
-    }
+//    private void OnEnable()
+//    {
+//		playGif ();
+//    }
 
-	private void OnDisable()
-	{
-		cycling = false;
-	}
+//	private void OnDisable()
+//	{
+//		cycling = false;
+//	}
 
-    // Update is called once per frame
-    void Update () {
-//		if (cycling) {
-//			playGif ();
-//		}
-	}
-}
+//    // Update is called once per frame
+//    void Update () {
+////		if (cycling) {
+////			playGif ();
+////		}
+//	}
+//}
