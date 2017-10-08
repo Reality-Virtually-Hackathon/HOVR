e768de268897105380ac35398d6ad632362591d8
Peter Iordanov <iordano2@illinois.edu>
2017-10-08 12:37:14 -0400

    first attempt at random bird movement

---
+++ Assets/Script/BirdController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+	private bool flying = false;
+	private Vector3 dir;
+	private float speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+		dir = new Vector3(Random.Range(0f, 10.0f), Random.Range(0f, 10.0f), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flying) {
+			transform.rotation = Quaternion.LookRotation (dir);
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		}
+
+
+	}
+
+	public void startFlying() {
+		flying = true;
+	}
+
+}
--- Assets/Script/BirdRotate.cs
+++ Assets/Script/BirdRotate.cs
@@ -6,25 +6,29 @@ public class BirdRotate : MonoBehaviour {
 
     float speed = 50f;
     bool flying = false;
-
+	private Component[] birds;
 	// Use this for initialization
 	void Start () {
-		
+		birds = GetComponentsInChildren<BirdController>();
 	}
 
     void OnTriggerEnter(Collider other)
     {
         flying = true;
         Debug.Log("Hit birds");
+		foreach (BirdController bird in birds) {
+			bird.startFlying ();
+		}
     }
 
 
     // Update is called once per frame
     void Update () {
-        transform.Rotate(Vector3.up, speed * Time.deltaTime);
-        if (flying)
-        {
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y + 0.01f, transform.localPosition.z);
-        }
+        
+		if (flying) {
+			transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y + 0.01f, transform.localPosition.z);
+		} else {
+			transform.Rotate(Vector3.up, speed * Time.deltaTime);
+		}
     }
 }
